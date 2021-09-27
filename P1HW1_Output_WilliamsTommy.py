2f1b7ffa7af162d94de9219fb3f96fcbb0d2028a
Williamt2413 <90863044+Williamt2413@users.noreply.github.com>
2021-09-26 23:01:27 -0400

    Add files via upload

---
+++ P1HW1_Output_WilliamsTommy.py
@@ -0,0 +1,22 @@
+
+#9/22/21
+#Cti-110 P1H1 
+#Tommy Williams
+#
+
+user_num = int(input('Enter integer:\n'))
+
+
+
+
+print("You entered:", user_num)
+
+
+print(user_num, "squared is", user_num * user_num )
+
+print("And", user_num, "cubed is", user_num*user_num*user_num, "!!")
+
+user_num2 = int(input("Enter another integer:\n"))
+
+print(user_num, "+", user_num2, "is", user_num + user_num2)
+print(user_num, "*", user_num2, "is", user_num * user_num2)
